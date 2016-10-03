9caa9139b9a5ded6fc7abab17dff0f4f7e913b3e
uohzxela <uohzxela@gmail.com>
2016-10-03 16:23:22 +0800

    train more models with different params

--- test.py
+++ test.py
@@ -1,7 +1,51 @@
 import gensim, logging, os
 
-model = gensim.models.Word2Vec.load_word2vec_format('vectors', binary=False)
-print model.similar_by_word("FuncDef", topn=20)
+VECTORS_FILE = "vectors_10_100_inorder"
+TSNE_FILE = "tsne_" + "_".join(VECTORS_FILE.split("_")[1:]) + ".png"
+
+model = gensim.models.Word2Vec.load_word2vec_format(VECTORS_FILE, binary=False)
+print model.similar_by_word("While", topn=20)
+print model.doesnt_match("UnaryOp BinaryOp While TernaryOp".split())
+print model.doesnt_match("If For TernaryOp While Continue DoWhile".split())
+print model.doesnt_match("ID Constant IdentifierType FuncDef".split())
+
 # print model.similar_by_word("ExprList")
 # print model.similar_by_word("ID")
 # print model.similar_by_word("TernaryOp")
+
+
+labels = []
+X = []
+tokens = open('tokens')
+for token in tokens:
+	token = token.split(" ")[0]
+	labels.append(token)
+	X.append(model[token])
+
+def plot_with_labels(low_dim_embs, labels, filename=TSNE_FILE):
+  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+  plt.figure(figsize=(18, 18))  #in inches
+  for i, label in enumerate(labels):
+    x, y = low_dim_embs[i,:]
+    plt.scatter(x, y)
+    plt.annotate(label,
+                 xy=(x, y),
+                 xytext=(5, 2),
+                 textcoords='offset points',
+                 ha='right',
+                 va='bottom')
+
+  plt.savefig(filename)
+
+try:
+  from sklearn.manifold import TSNE
+  import matplotlib.pyplot as plt
+
+  tsne = TSNE(perplexity=5, n_components=2, init='pca', n_iter=5000)
+
+
+  low_dim_embs = tsne.fit_transform(X)
+  plot_with_labels(low_dim_embs, labels)
+
+except ImportError:
+  print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 import gensim, logging, os
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+import random
  
 # sentences = [['first', 'sentence'], ['second', 'sentence']]
 # # train word2vec on the two sentences
 # model = gensim.models.Word2Vec(sentences, min_count=1)
 
-class MyASTokens(object):
+def rearrange(line):
+	return line[1:len(line)/2+1] + [line[0]] + line[len(line)/2+1:]
+
+class AbstractSyntaxTokens(object):
     def __init__(self, dirname):
         self.dirname = dirname
  
@@ -14,9 +18,18 @@ class MyASTokens(object):
             for line in open(os.path.join(self.dirname, fname)):
             	if line[0].isdigit():
             		continue
-                yield line.split()
- 
-tokens = MyASTokens('ast_files') # a memory-friendly iterator
-model = gensim.models.Word2Vec(tokens)
-model.save_word2vec_format("vectors", fvocab="tokens", binary=False)
+            	line = line.split()
+            	if len(line) > 2:
+            		line = rearrange(line)
+                yield line
+
+SIZE = 100
+WINDOW = 10
+tokens = AbstractSyntaxTokens('ast_files') # a memory-friendly iterator
+model = gensim.models.Word2Vec(tokens, size=SIZE, window=WINDOW)
+try:
+	model.save_word2vec_format("vectors_" + str(WINDOW) + "_" + str(SIZE) + "_inorder", fvocab="tokens", binary=False)
+except:
+	model.save_word2vec_format("vectors_noname", fvocab="tokens", binary=False)
 # print model.similar_by_word('ExprList')
+
